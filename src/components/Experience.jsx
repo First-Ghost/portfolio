ea9f7b04687068eacbdd0b25cfcf4ba634145ce0
Fermented-coder <kuivalappa@gmail.com>
2024-10-15 13:29:54 +0300

    esperences dektop tablet and mobile size done

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,3 +1,37 @@
 export const Experience = () => {
-  return <div>Experience</div>;
+  return (
+    <div className=" lg:max-w-[1440px] lg:h-[800px] md:w-[704px] md:h-[451px] mx-auto flex justify-center items-start   ">
+      <div className="lg:w-[1110px] lg:h-[330px] border-t-2 border-[#e8ecec] flex flex-col justify-end     ">
+        <div
+          className="lg:w-[1100px] lg:h-[255px] md:w-[704px] md:h-[451px] w-[345px] h-[604px]
+  grid lg:grid-rows-2 lg:grid-cols-3 md:grid-rows-3 md:grid-cols-2 grid-cols-1 grid-rows-6 md:text-left text-center"
+        >
+          <div className="w-[345px] md:h-[98px] h-[67px]  flex flex-col justify-between px-2  ">
+            <h1 className="md:text-[48px] text-[32px] font-bold">HTML</h1>
+            <p>1 Years Experience</p>
+          </div>
+          <div className="w-[345px] md:h-[98px] h-[67px]  flex flex-col justify-between px-2 ">
+            <h1 className="md:text-[48px]  text-[32px] font-bold">CSS</h1>
+            <p>1 Years Experience</p>
+          </div>
+          <div className="w-[345px] md:h-[98px] h-[67px]  flex flex-col justify-between px-2 ">
+            <h1 className="md:text-[48px] text-[32px] font-bold">Sass</h1>
+            <p>2 Months Experience</p>
+          </div>
+          <div className="w-[345px] md:h-[98px] h-[67px]  flex flex-col justify-between px-2 ">
+            <h1 className="md:text-[48px] text-[32px] font-bold">Javascript</h1>
+            <p>1 Years Experience</p>
+          </div>
+          <div className="w-[345px] md:h-[98px] h-[67px] flex flex-col justify-between px-2 ">
+            <h1 className="md:text-[48px] text-[32px] font-bold">React</h1>
+            <p>1 Years Experience</p>
+          </div>
+          <div className="w-[345px] md:h-[98px]  h-[67px] flex flex-col justify-between px-2 ">
+            <h1 className="md:text-[48px] text-[32px] font-bold">Tailwind</h1>
+            <p>1 Years Experience</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,48 +1,60 @@
 import myImage from "../images/porto 2.jpg";
+import rings from "../images/pattern-rings.svg";
+
 import { TypeAnimation } from "react-type-animation";
 
 export const Hero = () => {
   return (
     <div
-      className="relative  lg:max-w-[1440px] lg:h-[800px] mx-auto flex 
-    justify-center items-center   "
+      className="relative lg:max-w-[1440px] lg:h-[800px] mx-auto flex 
+    justify-center items-center    "
     >
-      <div className="lg:w-[1340px] lg:h-[700px] flex   ">
-        <div className="lg:w-[680px] lg:h-[650px]  mx-auto px-4 flex flex-col  items-start  justify-center   ">
-          <h1 className=" font-bold leading-[88px] tracking-[-2.5px] lg:w-[300] lg:h-[200px]   my-8">
+      <div className="lg:w-[1340px] lg:h-[700px] md:w-[768px] md:h-[624px] md:mt-10 flex  ">
+        <div className="lg:w-[680px] lg:h-[650px] md:w-[668px] md:h-[466px]   mx-auto px-4 flex flex-col  items-start lg:justify-center  md:justify-between   ">
+          <h1
+            className=" lg:w-[300] lg:h-[200px] lg:text-[88px] font-bold lg:leading-[88px] lg:tracking-[-2.5px]  lg:my-8 
+          md:text-[62px] md:leading-[72px] md:tracking-[-2.05px]
+          "
+          >
             <TypeAnimation
               sequence={[" I’m Kasim! ", 3000, "Nice to meet you! ", 3000]}
               wrapper="div"
               speed={50}
-              style={{ fontSize: "88px", display: "inline-block" }}
               repeat={Infinity}
             />
           </h1>
-          <p className="my-5 ml-2 font-bold ">
-            Front-End Developer (Hobbyist) | Based in Turku, Finland
-          </p>
-          <p className="ml-2">
-            I am a self-taught front-end developer with a passion for building
-            engaging, user-friendly interfaces. Over time, I have developed
-            skills in JavaScript, CSS, SASS, React, and Tailwind CSS. While I
-            haven&apos;t worked professionally in the field yet, I&apos;ve been
-            honing my skills through personal projects and continuously learning
-            the latest technologies to stay updated with modern web development
-            trends.
-          </p>
-          <div className="mt-10 text-[15px] leading-[16px] tracking-[2.20px] my-5 ml-2 font-bold w-[125px] h-[25px] border-b-2  border-b-[#4EE1A0] hover hover:tracking-[3px] ">
-            <button className="hover:text-[#4EE1A0]  duration-200 ">
-              CONTACT ME
-            </button>
+          <div className="flex flex-col items-start ">
+            <p className="my-5 ml-2 font-bold ">
+              Front-End Developer (Hobbyist) | Based in Turku, Finland
+            </p>
+            <p className="ml-2">
+              I&apos;m a self-taught front-end developer with skills in
+              JavaScript, CSS, SASS, and a focus on React and Tailwind CSS. I
+              build user-friendly interfaces through personal projects and stay
+              updated with the latest technologies. Fluent in both English and
+              Finnish.
+            </p>
+            <div className="mt-10 text-[15px] leading-[16px] tracking-[2.20px] my-5 ml-2 font-bold w-[125px] h-[25px] border-b-2  border-b-[#4EE1A0] hover hover:tracking-[3px] ">
+              <button className="hover:text-[#4EE1A0]  duration-200 ">
+                CONTACT ME
+              </button>
+            </div>
           </div>
         </div>
         <div>
           <img
-            className="lg:w-[430px] lg:h-[650px] object-cover px-4  "
+            className="lg:w-[430px] lg:h-[650px] object-cover px-4 "
             src={myImage}
             alt="me"
           />
         </div>
+      </div>
+      <div>
+        <img
+          className="absolute lg:left-[-15%] lg:top-[35%] md:left-[-35%] md:top-[15%]  md:block hidden "
+          src={rings}
+          alt="rings"
+        />
       </div>
     </div>
   );
